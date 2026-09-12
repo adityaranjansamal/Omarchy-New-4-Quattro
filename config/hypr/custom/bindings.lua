7414d3a61b157435db89ed97ee55e179f82eafbc
Aditya Ranjan Samal <83838764+adityaranjansamal@users.noreply.github.com>
2026-09-13 01:57:39 +0530

    Add files via upload
    Update 1 omadock binds written

--- config/hypr/custom/bindings.lua
+++ config/hypr/custom/bindings.lua
@@ -7,8 +7,6 @@ o.bind("CTRL + UP", "Mission Control",
 o.bind("CTRL + DOWN", "Close Mission Control",
   "omarchy-shell shell hide io.github.andyweiboan.missioncontrol")
 
-o.bind("SUPER + D", "Toggle Dock", "omarchy-shell -q rosakodu.dock toggleReveal")
-
 -- Open Resources App Monitor with CTRL+SHIFT+ESCAPE
 o.bind("CTRL + SHIFT + Escape", "Resources", "resources")
 
@@ -17,3 +15,14 @@ o.bind("SUPER + Q", "Close window mac os style", hl.dsp.window.close())
 
 -- Close Active Window by ALT + F4 more like Windows
 o.bind("ALT + F4", "Close window traditional", hl.dsp.window.close())
+
+-- omadock config
+-- Toggle Dock Visibility
+o.bind("SUPER + D", "Toggle Omadock", "exec qs -p /usr/share/omarchy/shell ipc call omadock toggleVisibility")
+
+-- Minimize currently focused window to Omadock
+o.bind("SUPER + M", "Minimize focused window", "exec qs -p /usr/share/omarchy/shell ipc call omadock minimizeActive")
+
+-- Restore longest-parked window (FIFO)
+hl.unbind("SUPER + SHIFT + M")
+o.bind("SUPER + SHIFT + M", "Restore oldest minimized", "exec qs -p /usr/share/omarchy/shell ipc call omadock restoreLast")
