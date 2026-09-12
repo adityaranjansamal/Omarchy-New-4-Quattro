9c37b92adc4213752d7af6382a12ef2effe929f0
Aditya Ranjan Samal <83838764+adityaranjansamal@users.noreply.github.com>
2026-09-13 02:01:57 +0530

    Added Qogir-ubuntu-dark icon theme
    Added the icon pack Qogir-ubuntu-dark and merged adw-gtk3 code hook into one file. The previous file 20-adw-gtk3-theme.sh need not to be used.

---
+++ config/omarchy/hooks/theme-set.d/20-adw-theme-qogir-icons-set.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+gsettings set org.gnome.desktop.interface gtk-theme 'adw-gtk3-dark'
+gsettings set org.gnome.desktop.interface icon-theme 'Qogir-Ubuntu-Dark'
+
+mkdir -p ~/.config/gtk-3.0
+cat > ~/.config/gtk-3.0/settings.ini <<'EOF'
+[Settings]
+gtk-theme-name=adw-gtk3-dark
+gtk-icon-theme-name=Qogir-Ubuntu-Dark
+EOF
+
+mkdir -p ~/.config/gtk-4.0
+cat > ~/.config/gtk-4.0/settings.ini <<'EOF'
+[Settings]
+gtk-icon-theme-name=Qogir-Ubuntu-Dark
+EOF
+
+gtk-update-icon-cache -f /usr/share/icons/Qogir-Ubuntu-Dark 2>/dev/null
